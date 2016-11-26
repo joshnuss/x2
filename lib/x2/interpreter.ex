323b8b8149f986b866a2c7321f4b0ec33e99e09e
Josh Nussbaum <joshnuss@gmail.com>
2016-11-26 01:42:19 -0500

    Adds error for unknown syntax

--- lib/x2/interpreter.ex
+++ lib/x2/interpreter.ex
@@ -1,4 +1,7 @@
 defmodule X2.Interpreter do
   def eval({op, a, b}) when op == :+ or op == :-,
     do: apply(Kernel, op, [a, b])
+
+  def eval(unknown),
+    do: raise RuntimeError, "Unknown syntax #{inspect unknown}"
 end
--- test/interpreter_test.exs
+++ test/interpreter_test.exs
@@ -3,6 +3,12 @@ defmodule X1.Interpreter.Test do
 
   import X2.Interpreter
 
+  test "errors when syntax unknown" do
+    assert_raise RuntimeError, ~r/Unknown/, fn ->
+      eval(:oops)
+    end
+  end
+
   describe "literal" do
     test "addition" do
       assert eval({:+, 1, 2}) == 3
